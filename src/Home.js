06c05dcbc248d1f7eb323965ee6fe77d16b39c66
sakthiofficial <sakthiroky123@gmail.com>
2022-12-10 11:34:43 +0530

    compeleted

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import IconButton from '@mui/material/IconButton';
 import GroupIcon from '@mui/icons-material/Group';
 import Button from '@mui/material/Button';
@@ -17,8 +17,6 @@ import { Bar } from "react-chartjs-2";
 import { useNavigate } from 'react-router';
 
 
-
-
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -35,14 +33,12 @@ export const options = {
 
 const labels = ["students", "teachers"];
 
-
-
 export const data = {
   labels,
   datasets: [
     {
       label:"School Management",
-      data: [100,50],
+      data: ["100", "150"],
       backgroundColor: "#d81b60"
     },
     
@@ -50,58 +46,34 @@ export const data = {
   ]
 };
 export function Home() {
-
-  let [student,setstudent]= useState(null)
-  let [teacher,setteacher]= useState(null)
-
-
-  function getStudentData (){
-    fetch("https://63899fdc4eccb986e895a926.mockapi.io/sampleapi").then(val=>val.json()).then(dt => setstudent(dt.length))
-  
-  }
-   useEffect(()=>getStudentData,[])
-   function getTeacherData (){ 
-    fetch("https://63899fdc4eccb986e895a926.mockapi.io/teachers").then(val=>val.json()).then(dt =>setteacher(dt.length))
-   }
-    useEffect(()=>{
-      getTeacherData()
-     },[])
- 
-return(
-  <div>
-  {student&teacher ? <HomeCont /> :"Loading ..."}
-  </div>
-)
-}
-function HomeCont(){
  
   return (
-    <div className="home-page">
-    <div className="home-page_btn">
-    <HomeInfo name={"Student"} path = {"/student"}icon ={<GroupIcon />}/>
-    <HomeInfo name={"Teacher"}path = {"/teacher"} icon ={<Person4Icon />}/>
+<div className="home-page">
+<div className="home-page_btn">
+<HomeInfo name={"Student"} path = {"/student"}icon ={<GroupIcon />}/>
+<HomeInfo name={"Teacher"}path = {"/teacher"} icon ={<Person4Icon />}/>
+
+
+
+</div>
+<HomeChart/>
+
+</div>
+    );
+}
+function HomeChart() {
+  return <Bar options={options} data={data} />;
+}
+
+function HomeInfo({name,icon,path}){
+  let navigate = useNavigate()
+  return(
+<div className="btns">
+     
     
-    
-    
+  <Button variant="contained" onClick={()=>navigate(path)} endIcon={icon}>
+  {name}
+</Button>
     </div>
-    <HomeChart/>
-    
-    </div>
-        );
-    }
-    function HomeChart() {
-      return <Bar options={options} data={data} />;
-    }
-    
-    function HomeInfo({name,icon,path}){
-      let navigate = useNavigate()
-      return(
-    <div className="btns">
-         
-        
-      <Button variant="contained" onClick={()=>navigate(path)} endIcon={icon}>
-      {name}
-    </Button>
-        </div>
-      )
+  )
 }
